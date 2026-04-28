20eb55c20d90b2faf637f0f39131c7c641bdb2c8
Rxdy <abcom@abend-core.dev>
2026-04-28 16:57:00 +0200

    chore: remove obsolete batch scripts for launching Abcom with logs

--- launch_debug.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM Lancer Abcom avec debug logging visible dans PowerShell
-powershell -NoExit -Command "cd '$env:LOCALAPPDATA\abcom'; Write-Host 'Lancement d''Abcom avec logging...' -ForegroundColor Green; Write-Host 'Clique sur le bouton + et regarde les logs' -ForegroundColor Yellow; & '.\abcom_debug.exe' 'ra' 2>&1 | ForEach-Object { Write-Host $_ }"
-
-
-
-
