f756455368395176bdc62d44c09593bdcc9c2822
Rxdy <abcom@abend-core.dev>
2026-04-28 16:57:00 +0200

    chore: remove obsolete batch scripts for launching Abcom with logs

--- run_with_logs.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Launch Abcom with logs redirected to a file
-cd /d "%LOCALAPPDATA%\abcom"
-echo Launching Abcom with logging...
-abcom_new.exe %USERNAME% > logs.txt 2>&1
-pause
--- test_modal.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM Lancer la version de test d'Abcom avec la modal TOUJOURS visible
-cd /d "%LOCALAPPDATA%\abcom"
-echo Lancement de la version de TEST (modal toujours visible)
-echo Si tu vois la modal "Créer un groupe - TEST", c'est que le problème est avec le button/condition
-echo Sinon, c'est un problème avec l'affichage de la modal elle-même
-echo.
-abcom_test.exe ra
